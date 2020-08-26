e4b3a1cb4fac6802878091206f311889bb3e4838
munkevin <xion861204@gmail.com>
2020-08-27 00:56:40 +0800

    initial upload

---
+++ lib/amplifyconfiguration.dart
@@ -0,0 +1,60 @@
+const amplifyconfig = ''' {
+    "UserAgent": "aws-amplify-cli/2.0",
+    "Version": "1.0",
+    "auth": {
+        "plugins": {
+            "awsCognitoAuthPlugin": {
+                "UserAgent": "aws-amplify-cli/0.1.0",
+                "Version": "0.1.0",
+                "IdentityManager": {
+                    "Default": {}
+                },
+                "CredentialsProvider": {
+                    "CognitoIdentity": {
+                        "Default": {
+                            "PoolId": "ap-southeast-1:f327190f-80e0-48d7-a86d-b063f1dabe71",
+                            "Region": "ap-southeast-1"
+                        }
+                    }
+                },
+                "CognitoUserPool": {
+                    "Default": {
+                        "PoolId": "ap-southeast-1_B5HUnFSoN",
+                        "AppClientId": "24de1ooij8sodv5gi13rer0gu3",
+                        "AppClientSecret": "o2sq7k18pb4bdnatt6t4g4vsqht7vtuhnhggtjrqe0sb5kj03ha",
+                        "Region": "ap-southeast-1"
+                    }
+                },
+                "Auth": {
+                    "Default": {
+                        "authenticationFlowType": "USER_SRP_AUTH"
+                    }
+                },
+                "PinpointAnalytics": {
+                    "Default": {
+                        "AppId": "db3d6467c95244dd818b5aee4112a5dc",
+                        "Region": "us-west-2"
+                    }
+                },
+                "PinpointTargeting": {
+                    "Default": {
+                        "Region": "us-west-2"
+                    }
+                }
+            }
+        }
+    },
+    "analytics": {
+        "plugins": {
+            "awsPinpointAnalyticsPlugin": {
+                "pinpointAnalytics": {
+                    "appId": "db3d6467c95244dd818b5aee4112a5dc",
+                    "region": "us-west-2"
+                },
+                "pinpointTargeting": {
+                    "region": "us-west-2"
+                }
+            }
+        }
+    }
+}''';
---
+++ lib/main.dart
@@ -0,0 +1,116 @@
+import 'package:amplify_auth_cognito/amplify_auth_cognito.dart';
+import 'package:flutter/material.dart';
+import 'package:amplify_core/amplify_core.dart';
+import 'package:flutter_app_poseidon/signin.dart';
+import 'package:flutter_app_poseidon/signup.dart';
+import 'package:flutter_login/flutter_login.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+import 'amplifyconfiguration.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: MyApp(),
+    routes: {
+      '/secondpage': (_) => SignupPage(),
+    },
+  ));
+}
+
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  bool _amplifyConfigured = false;
+  Duration get loginTime => Duration(milliseconds: 2250);
+  bool _isSignin = false;
+  String email;
+  // Instantiate Amplify
+  Amplify amplifyInstance = Amplify();
+  final emailController = TextEditingController();
+  final passwordController = TextEditingController();
+
+  @override
+  void initState() {
+    super.initState();
+    if (!_amplifyConfigured) _configureAmplify();
+  }
+
+  void _configureAmplify() async {
+    if (!mounted) return;
+
+    AmplifyAuthCognito authPlugin = AmplifyAuthCognito();
+    amplifyInstance.addPlugin(authPlugins: [authPlugin]);
+
+    // Once Plugins are added, configure Amplify
+    await amplifyInstance.configure(amplifyconfig);
+    try {
+      setState(() {
+        _amplifyConfigured = true;
+        print('amplify configured: $_amplifyConfigured');
+      });
+    } catch (e) {
+      print(e);
+    }
+  }
+
+  Future<String> _signupUser(LoginData data) async {
+    Map<String, dynamic> userAttributes = {
+      'email': emailController.text,
+    };
+    SignUpResult res = await Amplify.Auth.signUp(
+        username: data.name,
+        password: data.password,
+        options: CognitoSignUpOptions(userAttributes: userAttributes));
+    print(res);
+    if (res.isSignUpComplete) {
+      Fluttertoast.showToast(
+          toastLength: Toast.LENGTH_SHORT,
+          msg: 'Signup successfully!',
+          backgroundColor: Colors.blue,
+          textColor: Colors.white);
+      return null;
+    }
+    return 'error';
+  }
+
+  Future<String> _authUser(LoginData data) async {
+    SignInResult res = await Amplify.Auth.signIn(
+      username: data.name,
+      password: data.password,
+    );
+    if (res.isSignedIn) {
+      _isSignin = true;
+      email = data.name;
+      return null;
+    }
+    return 'error';
+  }
+
+  void _gotoSecondPage() async {
+    await Navigator.push(
+      context,
+      MaterialPageRoute(
+          builder: (_) => _isSignin
+              ? SigninPage(
+                  email: email,
+                )
+              : SignupPage()),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: FlutterLogin(
+        title: 'POSEIDON',
+        logo: 'assets/poseidon.jpg',
+        onLogin: _authUser,
+        onSignup: _signupUser,
+        onSubmitAnimationCompleted: _gotoSecondPage,
+        onRecoverPassword: (_) => null,
+      ),
+    );
+  }
+}
